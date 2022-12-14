1a1e13926250dab0d4755846783be2b706982663
Sanjay9783 <sanjayav8397@gmail.com>
2022-12-14 15:14:04 +0530

    update

--- setup.py
+++ setup.py
@@ -17,13 +17,9 @@ def get_requirements()->List[str]:
 
 setup(
     name="sensor",
-    version="0.0.2",
+    version="0.0.1",
     author="Sanjay_A_V",
     author_email="sanjayav8397@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
