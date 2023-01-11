17c67565c305f76c50313195b383ba1814309aed
Sanjay9783 <sanjayav8397@gmail.com>
2023-01-11 16:21:49 +0530

    update

--- understanding.py
+++ understanding.py
@@ -11,7 +11,8 @@ class employee:
     def __init(self,emp_name,age):
         self.emp_name:emp_name
         self.age:age
-##### git
+
+### git
 '''
 git config --global user.email "sanjayav8397@gmail.com"
 git config --global user.name "Sanjay9783"
@@ -51,7 +52,6 @@ ENTRYPOINT [ "/bin/sh" ]
 CMD ["start.sh"]
 '''
 
-
 ##### steps for deployment #####
 ''' class = 11 dec
 step 1:- mogodb atlas (dumping data to mongodb web)
@@ -59,8 +59,8 @@ step 1:- mogodb atlas (dumping data to mongodb web)
             # connet mongodb ip address 0.0.0.0/0
             # copy mongodb atlas url
             mongodb atlas url: mongodb+srv://sanjay:sanjay@cluster0.ua1lgcn.mongodb.net/?retryWrites=true&w=majority
-            insert dataset to mongodb atlas (by running data_dump.py)
-            # test code localy once after dumping data to mongodb atlas
+                insert dataset to mongodb atlas (by running data_dump.py)
+                # test code localy once after dumping data to mongodb atlas
 
 step 2:-create 'Docker file' (time = 1.49 - 2.01)
         create 'start.sh'
@@ -69,81 +69,78 @@ step 2:-create 'Docker file' (time = 1.49 - 2.01)
         create 'training_pipeline.py,batch_prediction.py' file in new folder airflow/dags (time = 2.02 - 2.16)
         create 'main.yaml' in new folder .github/workflow (time = 2.16 - 2.38)
 
-step 3:- set all secret keys to main.yaml file
+step 3:- set all secret keys required for main.yaml file
         ---> from IAM aws
-        AWS_ACCESS_KEY_ID = AKIA3FISYO36HEZM2KPJ
-        AWS_SECRET_ACCESS_KEY = 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
-        AWS_REGION = ap-south-1
+            AWS_ACCESS_KEY_ID = AKIA3FISYO36MAV3YRAA
+            AWS_SECRET_ACCESS_KEY = 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
         ---> from ECR aws
-        AWS_ECR_LOGIN_URI = 767227885308.dkr.ecr.ap-south-1.amazonaws.com
-        ECR_REPOSITORY_NAME = sensor-fault
+            AWS_ECR_LOGIN_URI = 767227885308.dkr.ecr.ap-south-1.amazonaws.com/
+            ECR_REPOSITORY_NAME = sensor_fault
         ---> from s3 aws
-        BUCKET_NAME = aps-fault-sensor
+            BUCKET_NAME = aps-fault-sensor
+        AWS_REGION = ap-south-1
         MONGO_DB_URL = mongodb+srv://sanjay:sanjay@cluster0.ua1lgcn.mongodb.net/?retryWrites=true&w=majority
 
-    in aws create new user by searching-->IAM-->user-->add_user-->name-->admin_acess-->download csv
+    in aws create new user by searching-->IAM (time  = 2.45)
+        -->user-->add_user-->give_name + access_key-->(attach existing policies directly)AdministratorAcess-->download csv
+                inside downloaded csv we will get = (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION)
+    in aws search 'ecr' (time = 2.56)
+        -->repositry-->change location to mumbai at top corner--> create repo (ap-south-1)
+                after creating repo copy repo url (767227885308.dkr.ecr.ap-south-1.amazonaws.com/sensor_fault)
+                    in url we will get = (AWS_ECR_LOGIN_URI,ECR_REPOSITORY_NAME)
+    in aws search s3 (time = 3.00 - 3.02)
+        --> buckets --> create bucket
 
-    in aws search 'ecr' -->repositry-->change location to mumbai at top corner--> create repo
+step 4:- EC2 --> WE NEED TO CREATE EC2 machine (time = 3.04.50 - 3.19 )
+    in aws search ec2
+        --->instances-->launch_instances-->give name, choose ubuntu, create_key(Create new key pair-->give name), Configure storage=30gb-->launch_instances
+        --->select your created instance-->security-->click on 'security groups'-->in 'inbound rules'-->Edit inbound rules-->'Add rule'-->choose ('All traffic','Anywhere-ipv4')-->save_rules
+        --->select your created instance-->instance-->connect-->connect--> it will open new tab 'EC2 Instance Connect'
 
-    in aws search s3--> create bucket
+step 5:-(time = 3.19 - 3.25)
+    after step4  we will get a consol/tab('EC2 Instance Connect') exicute below code line by line to installdocker in aws-ubantu 
+        -->clear
+        -->curl -fsSL https://get.docker.com -o get-docker.sh
+        -->sudo sh get-docker.sh
+        -->sudo usermod -aG docker ubuntu
+        -->newgrp docker
+        -->docker --version
 
-step 4:- EC2 --> WE NEED TO CREATE
+step 6:-(time = 3.25 - )
+    open project repop in github-->setings-->actions-->runners-->'New self-hosted runner'-->linux----
+    ---> run commands below in ec2 machine one by one
+            ---> code in 'Download' tab
+            ---> code in 'Configure' tab
+                    after pasting 1st code hit--->enter (./config.sh --url https://github.com/Sanjay9783/aps-fault-detection --token A2FDSSFZOT4MNIKOJ7IB6SDDX2KOM)
+                        √ Connected to GitHub
+                        # Runner Registration
+                        -->Enter the name of the runner group to add this runner to: [press Enter for Default] --> enter
+                        -->Enter the name of runner: [press Enter for ip-172-31-45-106] -->'self-hosted'
+                        This runner will have the following labels: 'self-hosted', 'Linux', 'X64' 
+                        -->Enter any additional labels (ex. label-1,label-2): [press Enter to skip] -->enter
+                        √ Runner successfully added
+                        √ Runner connection is good
+                        # Runner settings
+                        -->Enter name of work folder: [press Enter for _work]-->enter
+                        Settings Saved.
+                    second code in configure "./run.sh"
 
-in aws ec2-->instances-->launch_instances-->give name, choose ubantu , create_key,  ,Configure storage=30gb-->launch_instances
-    select your instance-->security_tab-->click on security group-->in inbound rules--edit inbound rules-->add rule--chose all_traffic,anywhere-ipv4
-    instance-->connect-->connect
-
-step 5:-
-    after step6  we will get a consol exicute code line by line to installdocker in aws-ubantu
-    clear
-    #curl -fsSL https://get.docker.com -o get-docker.sh
-    #sudo sh get-docker.sh
-    #sudo usermod -aG docker ubuntu
-    #newgrp docker
-    docker --version
-
-step 6:-
-    open project repop in github-->setings-->actions-->runners-->new self-hosted runner-->linux----
-    ---> run commands below in ec2 machine one by one 
-                    configure in github--
-                        after first command -->enter -->type 'self-hosted'-->enter
-                            √ Connected to GitHub
-
-                            # Runner Registration
-
-                            Enter the name of the runner group to add this runner to: [press Enter for Default] 
-                                -->enter
-                            Enter the name of runner: [press Enter for ip-172-31-45-106] self-hosted
-                                -->self-hosted
-                            This runner will have the following labels: 'self-hosted', 'Linux', 'X64' 
-                            Enter any additional labels (ex. label-1,label-2): [press Enter to skip] 
-                                -->enter
-                            √ Runner successfully added
-                            √ Runner connection is good
-
-                            # Runner settings
-
-                            Enter name of work folder: [press Enter for _work]
-                                -->enter
-
-                            Settings Saved.
-
-                            ubuntu@ip-172-31-45-106:~/actions-runner$ ./run.sh
-
-step 7:-
-    github-->setings-->secrets-->actions-->new repo secret ----
+step 7:-(time ends = 3.50)
+    github-->setings-->secrets and variables-->actions-->new repo secret ----
             add one by one
-        ---> AWS_ACCESS_KEY_ID = AKIA3FISYO36HEZM2KPJ
-            AWS_SECRET_ACCESS_KEY = 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
+        --->AWS_ACCESS_KEY_ID = AKIA3FISYO36MAV3YRAA
+            AWS_SECRET_ACCESS_KEY = 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
             AWS_REGION = ap-south-1
-            AWS_ECR_LOGIN_URI = 767227885308.dkr.ecr.ap-south-1.amazonaws.com
-            ECR_REPOSITORY_NAME = sensor-fault
+            AWS_ECR_LOGIN_URI = 767227885308.dkr.ecr.ap-south-1.amazonaws.com/
+            ECR_REPOSITORY_NAME = sensor_fault
             BUCKET_NAME = aps-fault-sensor
             MONGO_DB_URL = mongodb+srv://sanjay:sanjay@cluster0.ua1lgcn.mongodb.net/?retryWrites=true&w=majority
 
-
 step 8:- 
-       if in github setings-->action-->runner(ofline)--> run(cd actions-runner , ./run.sh)
+        if in github setings-->action-->runner(ofline)
+            in EC2 instance Connect (tab/consol) run below code
+                -->cd actions-runner
+                -->./run.sh)
 
 ./config.sh --url https://github.com/Sanjay9783/aps-fault-detection --token A2FDSSDDGKGQOQ5QECDOFRLDUKENQ
 
